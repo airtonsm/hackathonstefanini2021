39681843b0a928e2b8b3d5d1b09f554f430d5d61
airtonsm <airtonsmorais28@gmail.com>
2021-04-21 19:19:43 -0300

    API REST nodejs

--- exercicios/arquivo_aula06_JoseAirton.js
+++ exercicios/arquivo_aula06_JoseAirton.js
@@ -2,9 +2,10 @@ const {EventEmitter} = require('events')
 
 const evento = new EventEmitter()
 
-var alunos = [{nomeAluno : "", dataNascimento : ""}]
+var alunos = [{nomeAluno : '', dataNascimento : ''}]
 
 evento.on('eventos', (nome, data) =>{
+    alunos.push({nomeAluno : nome, dataNascimento : data})
     alunos.sort
     console.table(alunos)
 })

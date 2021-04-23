9a3d3949466bae9a257ad8947a769bf2b2398dc1
airtonsm <airtonsmorais28@gmail.com>
2021-04-22 22:41:58 -0300

    aula06

--- exercicios/arquivo_aula06_JoseAirton.js
+++ exercicios/arquivo_aula06_JoseAirton.js
@@ -6,11 +6,11 @@ var alunos = [{nomeAluno : '', dataNascimento : ''}]
 
 evento.on('eventos', (nome, data) =>{
     alunos.push({nomeAluno : nome, dataNascimento : data})
-    alunos.sort
+    alunos.sort()
     console.table(alunos)
 })
 
-evento.emit('eventos', 'airton', '18/05/1963')
-evento.emit('eventos', 'Jorge', '18/05/1985')
 evento.emit('eventos', 'Filipe', '18/05/1988')
+evento.emit('eventos', 'Jorge', '18/05/1985')
+evento.emit('eventos', 'airton', '18/05/1963')
 
